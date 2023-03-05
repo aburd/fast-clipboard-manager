753172d93934207954309c04574afc7232811c13
Aaron Burdick <aaron.burdick@protonmail.com>
2023-03-05 22:36:12 +0900

    feat: stub out basic lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
+const APPLICATION_ID: &str = "com.github.aburd.tiled-clipboard-manager";
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id(APPLICATION_ID)
+        .build();
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(320)
+            .modal(true)
+            .decorated(false)
+            .build();
+
+        window.show_all();
+    });
+
+    app.run();
 }
