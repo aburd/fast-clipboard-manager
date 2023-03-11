3133facbc8cb440fd46220ab87263225ca2c1b3c
aburd <burdick.aaron@gmail.com>
2023-03-11 21:00:20 +0900

    feat: add css

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,12 @@
 use crate::clipboard;
-use crate::clipboard::Clipboard;
 use crate::config;
 use crate::os_clipboard::OsClipboard;
 use crate::APPLICATION_ID;
 use clipboard_master::Master;
+use gtk::gdk::Display;
 use gtk::glib;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, CssProvider, StyleContext};
 use gtk4 as gtk;
 use log::info;
 use std::sync::{Arc, Mutex};
@@ -28,6 +28,17 @@ pub fn build_app() -> Result<Application, AppError> {
 }
 
 fn build_ui(app: &gtk::Application) {
+    // The CSS "magic" happens here.
+    let provider = CssProvider::new();
+    provider.load_from_data(include_str!("styles.css"));
+    // We give the CssProvided to the default screen so the CSS rules we added
+    // can be applied to our window.
+    StyleContext::add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
+
     let window = ApplicationWindow::builder()
         .application(app)
         .default_width(720)
@@ -44,10 +55,15 @@ fn build_ui(app: &gtk::Application) {
     let clipboard = Arc::new(Mutex::new(clipboard::get_clipboard(&config).unwrap()));
     let clipboard_cm = Arc::clone(&clipboard);
 
+    let body = gtk::Box::new(gtk::Orientation::Vertical, 10);
+    let title = gtk::Label::new(None);
+    title.set_text("Clipboard");
     let label = gtk::Label::new(None);
     label.set_text(&clipboard.lock().unwrap().entries_text());
 
-    window.set_child(Some(&label));
+    body.append(&title);
+    body.append(&label);
+    window.set_child(Some(&body));
 
     window.show();
 
