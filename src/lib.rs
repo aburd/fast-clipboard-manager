0bd40a3343be6a9d4ada2384b4dcc2d142aca02d
aburd <burdick.aaron@gmail.com>
2023-03-12 15:57:02 +0900

    chore: delete unused

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 pub mod app;
 pub mod clipboard;
 pub mod config;
-pub mod os_clipboard;
 
 pub const APPLICATION_ID: &str = "com.github.aburd.fast-clipboard-manager";
-pub use os_clipboard::OsClipboard;
